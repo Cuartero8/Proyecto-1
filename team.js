5d82006661cf6f019ff2a92a28d33c0bfd30fc21
Paul Castillo <pol.paul.207@gmail.com>
2023-11-14 19:05:16 +0100

    este es mi documento

--- team.js
+++ team.js
@@ -1,20 +1,14 @@
 let estudiantes = [
-{
-    name: "", surname: "", age: "", city: "",hobby: "", favoriteFood: "", favoriteVideoGame: "",
-    favoriteFilm: "", favoriteBook: "", petName: "",
-},
-  {name: "Carlos", surname: "Cuartero", age: 26, city: "Zaragoza", hobby: ["Futsal", "Mangaku", "Videojuegos", "Fiestuki"], 
-                favoriteFood: "Pizza", favoriteVideoGame: "Fire Emblem", favoriteFilm: "LoR", favoriteBook: "Soy analfabeto", petName: "Cuquin"},
   {
-    name: "Christian",
-    surname: "Hernandez",
+    name: "paul",
+    surname: "castillo",
     age: 25,
-    city: "Vitoria-Gasteiz",
-    hobby: "Videojuegos",
-    favoriteFood: "Lasagna",
-    favoriteVideoGame: "Pokemon",
-    favoriteFilm: "Harry Potter",
-    favoriteBook: "Harry Potter",
+    city: "madrid",
+    hobby: "futbol",
+    favoriteFood: "pasta",
+    favoriteVideoGame: "fifa",
+    favoriteFilm: "",
+    favoriteBook: "",
     petName: ""
     }
 ];
