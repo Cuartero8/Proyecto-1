88a266c384a91554b70b723d86e0a5141f3fb0d9
Monica Guerra <monik_flute@hotmail.com>
2023-11-14 18:51:30 +0100

    cualquier cosa

--- team.js
+++ team.js
@@ -4,3 +4,24 @@ let estudiantes = [
     favoriteFilm: "", favoriteBook: "", petName: "",
 }
 ];
+
+let estudiantes = [
+    {
+        name: "", surname: "", age: "", city: "",hobby: "", favoriteFood: "", favoriteVideoGame: "",
+        favoriteFilm: "", favoriteBook: "", petName: "",
+    },
+      {name: "Carlos", surname: "Cuartero", age: 26, city: "Zaragoza", hobby: ["Futsal", "Mangaku", "Videojuegos", "Fiestuki"], 
+                    favoriteFood: "Pizza", favoriteVideoGame: "Fire Emblem", favoriteFilm: "LoR", favoriteBook: "Soy analfabeto", petName: "Cuquin"},
+      {
+        name: "Christian",
+        surname: "Hernandez",
+        age: 25,
+        city: "Vitoria-Gasteiz",
+        hobby: "Videojuegos",
+        favoriteFood: "Lasagna",
+        favoriteVideoGame: "Pokemon",
+        favoriteFilm: "Harry Potter",
+        favoriteBook: "Harry Potter",
+        petName: ""
+        }
+    ];
