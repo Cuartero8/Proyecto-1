e5735f0df658d879f46241651ed005b2140972bc
Christian <christianalexp@live.com>
2023-11-14 17:52:39 +0100

    Rama de Christian

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let estudiantes = [];
+let estudiantes = [
+    {
+    name: "Christian",
+    surname: "Hernandez",
+    age: 25,
+    city: "Vitoria-Gasteiz",
+    hobby: "Videojuegos",
+    favoriteFood: "Lasagna",
+    favoriteVideoGame: "Pokemon",
+    favoriteFilm: "Harry Potter",
+    favoriteBook: "Harry Potter",
+    petName: ""
+    }
+];
