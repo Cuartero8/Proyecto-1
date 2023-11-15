6b8beef8d4a1ef4ea3ab3e96c391d98facb1ba4b
Francisco Javier Martinez Rodriguez <68655644+ilParaguas@users.noreply.github.com>
2023-11-15 17:00:54 +0100

    Update team.js
    Me he equivocado al hacer merge

--- team.js
+++ team.js
@@ -1,14 +1,1 @@
-let fcojavier = {
-    name: "Francisco Javier",
-    surname: "Martinez Rodriguez",
-    age: 27,
-    city: "Badajoz",
-    hobby: "Videogames",
-    favoriteFood: "Kebab",
-    favoriteVideoGame: "Risk of Rain 2",
-    favoriteFilm: "Better call Saul",
-    favoriteBook: "Vigilantes",
-    petName: "",
-}
-
-let estudiantes = [fcojavier];
+let estudiantes = [];
