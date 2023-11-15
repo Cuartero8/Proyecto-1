eefc4f588d8ba56bc981b07373f066309bd6cd76
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2023-11-15 13:47:38 +0100

    Modificado el metodo de ordenar los estudiantes

--- team.js
+++ team.js
@@ -19,23 +19,26 @@ let estudiantes = [
     }
 ];
 
-function ordenarPorNombre(estudiantes=[]) {
-    let listaNombres = [];
+listaOrdenada = estudiantes.sort(function (a, b) {      // Método para ordenar las personas por apellido y nombre
+    if (a.surname > b.surname) {
+        return 1;
+    } else if (a.surname < b.surname) {
+        return -1;
+    } else if (a.surname === b.surname) {
+        if (a.name > b.name) {
+            return 1;
+        } else if (a.name < b.name) {
+            return -1;
+        } else { return 0; }
+    }
+    
+    }
+);
 
-    for (i in estudiantes) {    // Bucle para conseguir los nombres
-        listaNombres.push([estudiantes[i].surname, estudiantes[i].name]);
-    };
-
-    listaOrdenada = listaNombres.sort();    // Ordeno los nombres
-    console.log(listaOrdenada);
-
-    // console.log("Lista de equipo ordenada por Apellido-Nombre:");
-    // for (i in listaOrdenada) {   // Bucle para mostrar los nombres ordenados
-    //     for (o in estudiantes) {
-    //         if ()
-
-    //     }
-    // };
-};
-
-ordenarPorNombre(estudiantes);
+for (i in listaOrdenada) {      // Bucle para mostrar las personas ordenadas
+    console.log(`${listaOrdenada[i].name} ${listaOrdenada[i].surname}:`);
+    for (k in listaOrdenada[i]) {
+        process.stdout.write(`${k}: ${listaOrdenada[i][k]} `);
+    }
+    console.log();
+}
