9b735eb7fdd2ddd972385bdc27a1e7ba018f1cb0
Monica Guerra <monik_flute@hotmail.com>
2023-11-16 17:53:45 +0100

    cambio else

--- team.js
+++ team.js
@@ -1,23 +1,92 @@
 let estudiantes = [
+  {
+    name: "paul",
+    surname: "castillo",
+    age: 25,
+    city: "madrid",
+    hobby: "futbol",
+    favoriteFood: "pasta",
+    favoriteVideoGame: "fifa",
+    favoriteFilm: "",
+    favoriteBook: "",
+    petName: "",
+  },
+  {
+    name: "monica",
+    surname: "guerra",
+    age: 30,
+    city: "Tarragona",
+    hobby: "leer",
+    favoriteFood: "sushi",
+    favoriteVideoGame: "",
+    favoriteFilm: "",
+    favoriteBook: "",
+    petName: "",
+  },
+  {
+    name: "Carlos",
+    surname: "Cuartero",
+    age: 26,
+    city: "Zaragoza",
+    hobby: ["Futsal", "Mangaku", "Videojuegos", "Fiestuki"],
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "Fire Emblem",
+    favoriteFilm: "LoR",
+    favoriteBook: "Soy analfabeto",
+    petName: "Cuquin",
+  },
+  {
+    name: "Christian",
+    surname: "Hernandez",
+    age: 25,
+    city: "Vitoria-Gasteiz",
+    hobby: "Videojuegos",
+    favoriteFood: "Lasagna",
+    favoriteVideoGame: "Pokemon",
+    favoriteFilm: "Harry Potter",
+    favoriteBook: "Harry Potter",
+    petName: "",
+  },
+  {
+    name: "Francisco Javier",
+    surname: "Martinez Rodriguez",
+    age: 27,
+    city: "Badajoz",
+    hobby: "Videogames",
+    favoriteFood: "Kebab",
+    favoriteVideoGame: "Risk of Rain 2",
+    favoriteFilm: "Better call Saul",
+    favoriteBook: "Vigilantes",
+    petName: "",
+  },
+];
+
+const busqueda = estudiantes.reduce((acc, estudiantes) => {
+  acc[estudiantes.name] = ++acc[estudiantes.name] || 0;
+  return acc;
+}, {});
+
+const duplicados = estudiantes.filter((estudiantes) => {
+  return busqueda[estudiantes.name];
+});
+
+let nombreRepetido = (duplicados) => {
+  var segundo = true;
+  for (let i = 0; i <= duplicados.length - 1; i++) {
+    let list = duplicados[i];
+
+    if (
+      estudiantes.indexOf(estudiantes.name) != -1 &&
+      estudiantes.indexOf(estudiantes.name) == -1
+    )
+      repetidos.push(estudiantes.name);
     {
-        name: "monica", surname: "guerra", age:30, city: "Tarragona", hobby: "leer", favoriteFood: "sushi", favoriteVideoGame: "",
-        favoriteFilm: "", favoriteBook: "", petName: "",
-    },
-      {name: "Carlos", surname: "Cuartero", age: 26, city: "Zaragoza", hobby: ["Futsal", "Mangaku", "Videojuegos", "Fiestuki"], 
-                    favoriteFood: "Pizza", favoriteVideoGame: "Fire Emblem", favoriteFilm: "LoR", favoriteBook: "Soy analfabeto", petName: "Cuquin"},
-      {
-        name: "Christian",
-        surname: "Hernandez",
-        age: 25,
-        city: "Vitoria-Gasteiz",
-        hobby: "Videojuegos",
-        favoriteFood: "Lasagna",
-        favoriteVideoGame: "Pokemon",
-        favoriteFilm: "Harry Potter",
-        favoriteBook: "Harry Potter",
-        petName: ""
-        }
-    ];
-
-
-
+      segundo = false;
+      console.log(list.name, "esta repetido");
+    }
+  }
+  if (segundo) {
+    console.log("No hay nombres repetidos.");
+  }
+};
+nombreRepetido(duplicados);
