1b86618385200f13ede96f48a54592c7fd1c8f3a
Carlos Cuartero Tabuenca <133210965+Cuartero8@users.noreply.github.com>
2023-11-16 19:36:01 +0100

    Update LoL to team.js

--- team.js
+++ team.js
@@ -150,4 +150,17 @@ function orderByAge() {
   let estudiantesCopy = [...estudiantes];
   estudiantesCopy.sort((a, b) => a.age - b.age);
   estudiantesCopy.forEach((e) => console.log(`${e.name} ${e.age}`));
-}
+};
+
+let whoGame = (estudiantes) => {
+  for (let i = 0; i <= estudiantes.length - 1; i++) {
+    let list = estudiantes[i];
+
+    if (list.favoriteVideoGame === "LOL") {
+      console.log(list.name, "juega LOL");
+    } else if (list.favoriteVideoGame === "League Of Legends") {
+      console.log(list.name, "League Of Legends");
+    } 
+  }
+}; 
+whoGame(estudiantes);
