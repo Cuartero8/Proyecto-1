78cee5d85fc154f0723b6890fa9d29c3b882d150
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2023-11-16 16:43:04 +0100

    Commit para dejar Development como en pull

--- team.js
+++ team.js
@@ -1,5 +1,5 @@
 let estudiantes = [
-  {
+{
     name: "paul",
     surname: "castillo",
     age: 25,
@@ -10,58 +10,36 @@ let estudiantes = [
     favoriteFilm: "",
     favoriteBook: "",
     petName: ""
-    },
-    {
-        name: "monica", surname: "guerra", age:30, city: "Tarragona", hobby: "leer", favoriteFood: "sushi", favoriteVideoGame: "",
-        favoriteFilm: "", favoriteBook: "", petName: "",
-    },
-    {name: "Carlos", surname: "Cuartero", age: 26, city: "Zaragoza", hobby: ["Futsal", "Mangaku", "Videojuegos", "Fiestuki"], 
-            favoriteFood: "Pizza", favoriteVideoGame: "Fire Emblem", favoriteFilm: "LoR", favoriteBook: "Soy analfabeto", petName: "Cuquin"},
-    {   name: "Christian",
-        surname: "Hernandez",
-        age: 25,
-        city: "Vitoria-Gasteiz",
-        hobby: "Videojuegos",
-        favoriteFood: "Lasagna",
-        favoriteVideoGame: "Pokemon",
-        favoriteFilm: "Harry Potter",
-        favoriteBook: "Harry Potter",
-        petName: ""
-    },
-    {
-      name: "Francisco Javier",
-      surname: "Martinez Rodriguez",
-      age: 27,
-      city: "Badajoz",
-      hobby: "Videogames",
-      favoriteFood: "Kebab",
-      favoriteVideoGame: "Risk of Rain 2",
-      favoriteFilm: "Better call Saul",
-      favoriteBook: "Vigilantes",
-      petName: ""
-    },
-  ];
-
-  listaOrdenada = estudiantes.sort(function (a, b) {      // Método para ordenar las personas por apellido y nombre
-    if (a.surname > b.surname) {
-        return 1;
-    } else if (a.surname < b.surname) {
-        return -1;
-    } else if (a.surname === b.surname) {
-        if (a.name > b.name) {
-            return 1;
-        } else if (a.name < b.name) {
-            return -1;
-        } else { return 0; }
-    }
-    
-    }
-);
-
-for (i in listaOrdenada) {      // Bucle para mostrar las personas ordenadas
-    console.log(`${listaOrdenada[i].name} ${listaOrdenada[i].surname}:`);
-    for (k in listaOrdenada[i]) {
-        process.stdout.write(`${k}: ${listaOrdenada[i][k]} `);
-    }
-    console.log();
-}
+},
+{
+    name: "monica", surname: "guerra", age:30, city: "Tarragona", hobby: "leer", favoriteFood: "sushi", favoriteVideoGame: "",
+    favoriteFilm: "", favoriteBook: "", petName: "",
+},
+{   
+    name: "Carlos", surname: "Cuartero", age: 26, city: "Zaragoza", hobby: ["Futsal", "Mangaku", "Videojuegos", "Fiestuki"], 
+    favoriteFood: "Pizza", favoriteVideoGame: "Fire Emblem", favoriteFilm: "LoR", favoriteBook: "Soy analfabeto", petName: "Cuquin"
+},
+{   name: "Christian",
+    surname: "Hernandez",
+    age: 25,
+    city: "Vitoria-Gasteiz",
+    hobby: "Videojuegos",
+    favoriteFood: "Lasagna",
+    favoriteVideoGame: "Pokemon",
+    favoriteFilm: "Harry Potter",
+    favoriteBook: "Harry Potter",
+    petName: ""
+},
+{
+    name: "Francisco Javier",
+    surname: "Martinez Rodriguez",
+    age: 27,
+    city: "Badajoz",
+    hobby: "Videogames",
+    favoriteFood: "Kebab",
+    favoriteVideoGame: "Risk of Rain 2",
+    favoriteFilm: "Better call Saul",
+    favoriteBook: "Vigilantes",
+    petName: ""
+},
+]
