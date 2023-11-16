0b56bf419188844655bbc989db6806c4eeb61fb9
Carlos Cuartero Tabuenca <cuarterodosmilocho@gmail.com>
2023-11-16 16:13:49 +0100

    Inicio de dia

--- team.js
+++ team.js
@@ -17,28 +17,4 @@ let estudiantes = [
     favoriteBook: "Harry Potter",
     petName: ""
     }
-];
-
-listaOrdenada = estudiantes.sort(function (a, b) {      // Método para ordenar las personas por apellido y nombre
-    if (a.surname > b.surname) {
-        return 1;
-    } else if (a.surname < b.surname) {
-        return -1;
-    } else if (a.surname === b.surname) {
-        if (a.name > b.name) {
-            return 1;
-        } else if (a.name < b.name) {
-            return -1;
-        } else { return 0; }
-    }
-    
-    }
-);
-
-for (i in listaOrdenada) {      // Bucle para mostrar las personas ordenadas
-    console.log(`${listaOrdenada[i].name} ${listaOrdenada[i].surname}:`);
-    for (k in listaOrdenada[i]) {
-        process.stdout.write(`${k}: ${listaOrdenada[i][k]} `);
-    }
-    console.log();
-}
+]
